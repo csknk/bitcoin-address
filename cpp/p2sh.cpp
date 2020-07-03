50c7239484263679cf26edda78a3081e93480422
David Egan <david@carawebs.com>
2020-07-03 09:06:39 +0100

    Add regtest option for p2sh binary

--- cpp/p2sh.cpp
+++ cpp/p2sh.cpp
@@ -7,10 +7,16 @@
 
 using namespace utilities;
 
-int main()
+int main(int argc, char *argv[])
 {
+	uint8_t networkByte = 0x00;
+	if (argc == 2 && std::string(argv[1]) == "-regtest") {
+		networkByte = 0xc4;
+	}
+
 	std::string hexstring;
-	std::cout << "Enter a hexstring representation of a P2SH <scriptPubkey>:\n";
+	std::cout << "Enter a hexstring representation of a public key,"
+		"or a hexstring representation of a P2SH multisig redeem script:\n";
 	std::cin >> hexstring;
 	Bytes input;
 	hexstringToBytes(hexstring, input);
@@ -21,8 +27,7 @@ int main()
 	ripemd160(&initialSHA256[0], initialSHA256.size(), ripemd160Hash);
 	
 	// Prepend the version byte
-	//ripemd160Hash.insert(std::begin(ripemd160Hash), 0x00);
-	ripemd160Hash.insert(std::begin(ripemd160Hash), 0xc4);
+	ripemd160Hash.insert(std::begin(ripemd160Hash), networkByte);
 
 	// Perform SHA256 twice  - checksum is the first four bytes of the result.
 	Bytes checksumHash(SHA256_DIGEST_LENGTH);
